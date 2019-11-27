0b5842936615a44babc2d5bd2e2f5026f5e1aa8c
devxu <devxu@thoughtworks.com>
2019-11-27 21:14:01 +0800

    [refactor] use the ticket class to locker

--- src/locker.js
+++ src/locker.js
@@ -1,26 +1,23 @@
-import shortid from "shortid";
-import _ from 'lodash';
+import Ticket from "./ticket";
 
 class Locker {
     constructor() {
         this.capacity = 100;
         this.available = this.capacity;
-        this.tickets = [];
+        this.ticket = new Ticket();
     }
 
     store() {
         if (this.available) {
             this.available --;
-            const ticket = shortid.generate();
-            this.tickets.push(ticket);
-            return ticket
+            return this.ticket.create();
         }
         return 'No available cell';
     }
 
     pickup(ticket) {
-        if(_.includes(this.tickets, ticket)) {
-            _.pull(this.tickets, ticket);
+        const destroy = this.ticket.destroy(ticket);
+        if(destroy) {
             this.available ++;
             return 'Picked successfully';
         }
