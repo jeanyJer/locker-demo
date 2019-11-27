682a605188febacb8d16c4609f2c4482cda1e046
devxu <devxu@thoughtworks.com>
2019-11-27 20:35:40 +0800

    [case1] delete the useless test

--- test/locker.spec.js
+++ test/locker.spec.js
@@ -4,14 +4,6 @@ import shortid from "shortid";
 describe('locker', () => {
     jest.spyOn(shortid, 'generate').mockReturnValue('shortid');
 
-    it.skip('should return a ticket when has available cell and store a package', () => {
-        const locker = new Locker();
-
-        const result = locker.store();
-
-        expect(result).toEqual('ticket');
-    });
-
     it('should return a random ticket when has available cell and store a package', () => {
         const locker = new Locker();
 
