7715407bb63bc781f5f5c57e5d495bb8f0270a55
devxu <devxu@thoughtworks.com>
2019-11-27 21:09:20 +0800

    [refactor] add destroy function to make the test case green

--- src/ticket.js
+++ src/ticket.js
@@ -1,4 +1,5 @@
 import shortid from "shortid";
+import _ from 'lodash';
 
 class Ticket {
     constructor() {
@@ -9,6 +10,13 @@ class Ticket {
         const ticket = shortid.generate();
         this.tickets.push(ticket);
         return ticket
+    }
+
+    destroy(ticket) {
+        if(_.includes(this.tickets, ticket)) {
+            _.pull(this.tickets, ticket);
+            return true;
+        }
     }
 }
 
